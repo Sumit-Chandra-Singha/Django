3a70160244171b94fdd382df1dc8dd95f193bc08
Sumit Chandra Singha <chandrasumit334@gmail.com>
2024-09-25 01:49:38 +0600

    added templates in first_app

--- project_2/first_app/views.py
+++ project_2/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("this is first_app/home page.")
+    # return HttpResponse("this is first_app/home page.")
+    return render(request, 'first_app/home.html')
