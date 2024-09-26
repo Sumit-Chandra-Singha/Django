3d51498fe01bdff6ad5ea68c38cb030d3bc97b06
Sumit Chandra Singha <chandrasumit334@gmail.com>
2024-09-27 00:33:48 +0600

    Django forms, Upload Files

--- project_6_forms/first_app/form.py
+++ project_6_forms/first_app/form.py
@@ -2,15 +2,16 @@ from django import forms
 
 class contact(forms.Form):
     name = forms.CharField(label="User Name")
-    email = forms.EmailField(label="User Email")
-    age = forms.IntegerField()
-    weight = forms.DecimalField()
-    balance = forms.FloatField()
-    check = forms.BooleanField()
-    birthday = forms.DateField()
-    appointment = forms.DateTimeField()
-    CHOICES = [('XS','EXTRA SMALL'),('S','SMALL'),('M','MEDIUM'),('L','LARGE'),('XL','EXTRA LARGE'),('XXL','EXTRA EXTRA LARGE')]
-    size = forms.ChoiceField(choices= CHOICES)
-    MEAL = [('P','PEPPERONI'),('M','MASHROOM'),('MY','MEYONEESE'),('C','CHEESE')]
-    meal = forms.MultipleChoiceField(choices= MEAL)
+    file = forms.FileField()
+    # email = forms.EmailField(label="User Email")
+    # age = forms.IntegerField()
+    # weight = forms.DecimalField()
+    # balance = forms.FloatField()
+    # check = forms.BooleanField()
+    # birthday = forms.DateField()
+    # appointment = forms.DateTimeField()
+    # CHOICES = [('XS','EXTRA SMALL'),('S','SMALL'),('M','MEDIUM'),('L','LARGE'),('XL','EXTRA LARGE'),('XXL','EXTRA EXTRA LARGE')]
+    # size = forms.ChoiceField(choices= CHOICES)
+    # MEAL = [('P','PEPPERONI'),('M','MASHROOM'),('MY','MEYONEESE'),('C','CHEESE')]
+    # meal = forms.MultipleChoiceField(choices= MEAL)
 
--- project_6_forms/first_app/views.py
+++ project_6_forms/first_app/views.py
@@ -18,7 +18,16 @@ def form(request):
     return render(request, 'form.html')
 
 def djangoform(request):
-    form = contact(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
+    if request.method == 'POST':
+        form = contact(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            with open('./first_app/upload/' + file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            print(form.cleaned_data)
+            return render(request, 'djangoform.html', {'form': form})
+    else:
+        form = contact()
+
     return render(request, 'djangoform.html', {'form': form})
