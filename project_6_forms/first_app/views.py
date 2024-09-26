c42d34cf8d5fc737b5cc01d1358e55bc35760ade
Sumit Chandra Singha <chandrasumit334@gmail.com>
2024-09-26 18:05:23 +0600

    Rating form, get request

--- project_6_forms/first_app/views.py
+++ project_6_forms/first_app/views.py
@@ -8,8 +8,8 @@ def about(request):
     if request.method == 'POST':
         name = request.POST.get('username')
         email = request.POST.get('email')
-        
-        return render(request, 'about.html',{'name': name, 'email': email})
+        select = request.POST.get('select')
+        return render(request, 'about.html',{'name': name, 'email': email, 'select': select})
 
     return render(request, 'about.html')
 
