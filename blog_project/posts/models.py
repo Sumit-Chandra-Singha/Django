edbec1bcc11c48d4ecb3f30b67bf4688748d18a4
Sumit Chandra Singha <chandrasumit334@gmail.com>
2024-09-28 15:04:45 +0600

    create models, make migrations, create superuser

--- blog_project/posts/models.py
+++ blog_project/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from catagories.models import Catagory
+from author.models import Author
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    catagory = models.ManyToManyField(Catagory)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Name : {self.title}, Author : {self.author}"
