46b6d0744f854c2314661051379e561448a6c4de
Sumit Chandra Singha <chandrasumit334@gmail.com>
2024-10-01 12:20:28 +0600

    register, login, logout

--- Blog_Project_Part_2/blog_project/author/urls.py
+++ Blog_Project_Part_2/blog_project/author/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register')
+    path('', views.register, name='register'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout')
 ]
--- Blog_Project_Part_2/blog_project/author/views.py
+++ Blog_Project_Part_2/blog_project/author/views.py
@@ -1,25 +1,59 @@
 from django.shortcuts import render, redirect
 from . import forms
- 
-# Create your views here.
-# def add_author(r):
-#     if r.method == 'POST':
-#         form = forms.Author_form(r.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('add_author')
-#     else:
-#         form = forms.Author_form()
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-#     return render(r, 'add_author.html',{'form': form})
-
-def register(r):
-    if r.method == 'POST':
-        form = forms.RegistrationForm(r.POST)
+def register(request):
+    if request.method == 'POST':
+        form = forms.RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Account Created Successfully.')
             return redirect('register')
     else:
         form = forms.RegistrationForm()
 
-    return render(r, 'register.html',{'form': form})
+    return render(request, 'register.html',{'form': form, 'type': 'Register'})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Logged In Successfully.')
+            return redirect('home')
+        else:
+            messages.warning(request, 'Login Information Incorrect.')
+            return redirect('user_login')
+    
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'register.html', {'form': form, 'type': 'LOGIN'})
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully.')
+    return redirect('user_login')
+
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, request.POST)
+#         if form.is_valid():
+#             user_name = form.cleaned_data['username']
+#             user_pass = form.cleaned_data['password']
+#             user = authenticate(username=user_name, password=user_pass)
+#             if user is not None :
+#                 messages.success(request, 'Logged In Successfully.')
+#                 login(request,user)
+#                 return redirect('user_login')
+#             else:
+#                 messages.warning(request, 'Login Information Incorrect.')
+#                 return redirect('register')
+    
+#     else:
+#         form = AuthenticationForm()
+#         return render(request, 'register.html', {'form': form, 'type': 'LOGIN'})
+#     # return render(request, 'register.html', {'form': form, 'type': 'LOGIN'})
